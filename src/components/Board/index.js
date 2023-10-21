3ecae6587bcdc8029df8c536939013270b7f5676
Shubham Malviya <you@example.com>
2023-10-22 00:49:43 +0530

    Add Undo / Redo feature in board

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -11,6 +11,9 @@ const Board = () => {
 	const canvasRef = useRef(null);
 	const shouldDraw = useRef(false);
 
+	const drawHistory = useRef([]);
+	const historyPointer = useRef(0);
+
 	// before browser paint
 	useLayoutEffect(() => {
 		if (!canvasRef.current) return;
@@ -42,11 +45,21 @@ const Board = () => {
 
 		const handleMouseUp = () => {
 			shouldDraw.current = false;
+			const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+			drawHistory.current.push(imageData);
+			historyPointer.current = drawHistory.current.length - 1;
+			console.log(historyPointer.current);
 		};
 
 		canvas.addEventListener("mousedown", handleMouseDown);
 		canvas.addEventListener("mousemove", handleMouseMove);
 		canvas.addEventListener("mouseup", handleMouseUp);
+
+		return () => {
+			canvas.removeEventListener("mousedown", handleMouseDown);
+			canvas.removeEventListener("mousemove", handleMouseMove);
+			canvas.removeEventListener("mouseup", handleMouseUp);
+		};
 	}, []);
 
 	useEffect(() => {
@@ -73,6 +86,19 @@ const Board = () => {
 			anchor.href = URL;
 			anchor.download = "sketch.jpg";
 			anchor.click();
+		} else if (actionMenuItem === MENU_ITEMS.UNDO || actionMenuItem === MENU_ITEMS.REDO) {
+			if (historyPointer.current > 0 && actionMenuItem === MENU_ITEMS.UNDO)
+				historyPointer.current -= 1;
+
+			if (
+				historyPointer.current < drawHistory.current.length - 1 &&
+				actionMenuItem === MENU_ITEMS.REDO
+			)
+				historyPointer.current += 1;
+
+			const imageData = drawHistory.current[historyPointer.current];
+			context.putImageData(imageData, 0, 0);
+			console.log(historyPointer.current);
 		}
 
 		dispatch(actionItemClick(null));
